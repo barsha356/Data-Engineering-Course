6e1f8f3f355de667a42f934921d050fcce2bb474
Barsha <barshapok8@gmail.com>
2026-07-18 17:35:04 +0545

    Complete Week 5 assignment

--- Week5/pipeline.py
+++ Week5/pipeline.py
@@ -29,7 +29,8 @@ from load import (
 )
 from transform import transform
 
-from quality  import run_quality_checks
+from quality import run_quality_checks
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Rides ETL pipeline")
@@ -40,6 +41,7 @@ def parse_args():
     )
     return parser.parse_args()
 
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s  %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
@@ -49,30 +51,29 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 SOURCE_DB_CONFIG = dict(
-    host=    os.getenv("SRC_DB_HOST"),
-    port =   os.getenv("SRC_DB_PORT"),
-    dbname = os.getenv("SRC_DB_NAME"),
-    user=    os.getenv("SRC_DB_USER"),
+    host=os.getenv("SRC_DB_HOST"),
+    port=os.getenv("SRC_DB_PORT"),
+    dbname=os.getenv("SRC_DB_NAME"),
+    user=os.getenv("SRC_DB_USER"),
     password=os.getenv("SRC_DB_PASSWORD")
 )
+
 DEST_DB_CONFIG = dict(
-    host=    os.getenv("DEST_DB_HOST"),
-    port =   os.getenv("DEST_DB_PORT"),
-    dbname = os.getenv("DEST_DB_NAME"),
-    user=    os.getenv("DEST_DB_USER"),
+    host=os.getenv("DEST_DB_HOST"),
+    port=os.getenv("DEST_DB_PORT"),
+    dbname=os.getenv("DEST_DB_NAME"),
+    user=os.getenv("DEST_DB_USER"),
     password=os.getenv("DEST_DB_PASSWORD")
 )
-
 
 
 def main():
     args = parse_args()
     mode = 'FULL' if args.full_reload else 'INCREMENTAL'
-    """
-    Extract all dimension data from the source DB and load them into the target DB.
-    """
+
     src_conn = psycopg2.connect(**SOURCE_DB_CONFIG)
     dst_conn = psycopg2.connect(**DEST_DB_CONFIG)
+
     try:
         time0 = time.time()
         driver_data = extract_driver(src_conn)
@@ -89,6 +90,7 @@ def main():
 
         promo_code_data = extract_promo_code(src_conn)
         load_dim_promo_code(dst_conn, promo_code_data)
+
         logger.info(f"Dimention table load completed on {time.time() - time0:.2f}s")
 
         time0 = time.time()
@@ -98,21 +100,28 @@ def main():
         time0 = time.time()
         if mode == 'INCREMENTAL':
             watermark = get_watermark(dst_conn)
-            rows = extract_trips_incremental(src_conn,{"watermark":watermark})
+            rows = extract_trips_incremental(src_conn, {"watermark": watermark})
         else:
             rows = extract_trips_full(src_conn)
+
         logger.info(f"Trip extraction  completed on {time.time() - time0:.2f}s")
 
         time0 = time.time()
         fact_rows = transform(rows, lookups)
         logger.info(f"Transformation completed on {time.time() - time0:.2f}s")
 
+        if not fact_rows:
+            logger.info("No new rows to process. Pipeline completed.")
+            return
+
         time0 = time.time()
         run_quality_checks(fact_rows)
         logger.info(f"Quality Check completed on {time.time() - time0:.2f}s")
+
         time0 = time.time()
         load_fact_trips(dst_conn, fact_rows)
         logger.info(f"Trip table load completed on {time.time() - time0:.2f}s")
+
     finally:
         src_conn.close()
         dst_conn.close()
@@ -120,4 +129,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
